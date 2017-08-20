ecce80954dbf92da29dcbf89e5f3a1099d6a6b89
Aritra Banerjee <oritro.75b@gmail.com>
2017-08-20 15:12:32 +0600

    created the django project api, app v1 and model for the database

---
+++ api/v1/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class Contacts(models.Model):
+    name = models.CharField(max_length=500)
+    msisdn = models.CharField(max_length=11)
+    address = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+    def get_msisdn(self):
+        return self.msisdn
+
+    def get_address(self):
+        return self.address
